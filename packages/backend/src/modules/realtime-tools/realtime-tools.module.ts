28bba6391533b5fe58e8744c909b900044994fdc
Mohammad Hamdallah <hamdallahmohammad3219@gmail.com>
2026-01-13 18:59:03 +0300

    Fix CalcomService injection for realtime tools

--- packages/backend/src/modules/realtime-tools/realtime-tools.module.ts
+++ packages/backend/src/modules/realtime-tools/realtime-tools.module.ts
@@ -6,9 +6,10 @@ import { KnowledgeModule } from '../knowledge/knowledge.module';
 import { RealtimeToolsController } from './realtime-tools.controller';
 import { RealtimeToolsService } from './realtime-tools.service';
 import { ToolsAuthGuard } from '../../common/guards/tools-auth.guard';
+import { CalcomModule } from '../../infrastructure/calcom/calcom.module';
 
 @Module({
-  imports: [CompaniesModule, AgentConfigModule, CompanyNumbersModule, KnowledgeModule],
+  imports: [CompaniesModule, AgentConfigModule, CompanyNumbersModule, KnowledgeModule, CalcomModule],
   controllers: [RealtimeToolsController],
   providers: [RealtimeToolsService, ToolsAuthGuard],
 })
